523177abf7bd7784907537f8b12a16acaf88ad28
bonklek <142337540+bonklek@users.noreply.github.com>
2026-07-04 12:10:29 -0400

    Restore release script workflow

---
+++ scripts/release/release-builds.mjs
@@ -0,0 +1,49 @@
+export const releaseBuilds = [
+  { dir: "dist/chromium", target: "chromium", profile: "full" },
+  { dir: "dist/firefox", target: "firefox", profile: "full" },
+];
+
+export const coreHostPermissions = [
+  "https://api.github.com/*",
+  "https://x.com/*",
+  "https://twitter.com/*",
+  "https://abs.twimg.com/*",
+  "https://pbs.twimg.com/*",
+  "https://publish.twitter.com/*",
+  "https://cdn.syndication.twimg.com/*",
+  "http://localhost/*",
+  "http://127.0.0.1/*",
+  "http://[::1]/*",
+  "https://remilia.wiki/*",
+  "https://wiki.remilia.org/*",
+];
+
+export const contentScriptMatches = [
+  "https://x.com/*",
+  "https://twitter.com/*",
+];
+
+export const webAccessibleMatches = [
+  ...contentScriptMatches,
+  "https://remilia.wiki/*",
+  "https://wiki.remilia.org/*",
+];
+
+export const commonAssetDirs = [
+  "brand",
+  "icons",
+  "remilia-fonts",
+];
+
+export const generatedAssetRoots = [
+  "generated",
+  "ocr",
+  "ort",
+];
+
+export const allowedMarkdownArchiveFiles = [
+  "ocr/core/README.md",
+  "wiki-helper/remilia-wiki-article-writer/SKILL.md",
+  "wiki-helper/remilia-wiki-article-writer/references/article-patterns.md",
+  "wiki-helper/remilia-wiki-article-writer/references/interview-guide.md",
+];
